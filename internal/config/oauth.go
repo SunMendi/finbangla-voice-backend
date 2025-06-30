13fa9289183184a21b64a25918f9034508d9afae
mehedi <mahbub@evidentbd.com>
2025-07-01 05:57:05 +0600

    Add RedirectURL to Google OAuth config

--- internal/config/oauth.go
+++ internal/config/oauth.go
@@ -12,7 +12,7 @@ func InitGoogleOAuth() {
 	GoogleOAuthConfig = &oauth2.Config{
         ClientID:     os.Getenv("GOOGLE_CLIENT_ID"),
         ClientSecret: os.Getenv("GOOGLE_CLIENT_SECRET"),
-        RedirectURL:  os.Getenv("GOOGLE_REDIRECT_URL"),
+        RedirectURL: "https://finbangla-voice-backend-production.up.railway.app/auth/google/callback",
         Scopes: []string{
             "https://www.googleapis.com/auth/userinfo.email",
             "https://www.googleapis.com/auth/userinfo.profile",
